876d50b675c7d0b8bf7d41ac0fe40551701a72e2
mahisokhary@gmail.com <mahisokhary@gmail.com>
2023-08-19 21:20:23 +0430

    test

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ window.onload = Function => {
   var time_res = document.getElementById("time-res");
   var name_in = document.getElementById("name-in");
   var name_submit = document.getElementById("name-submit");
+  var post_list = document.getElementById("post-list");
   
   console.log('Hello World!');
   text.innerHTML = "click me";
@@ -50,5 +51,21 @@ window.onload = Function => {
   
   name_submit.onclick = Function => {
     alert(name_in.value ? "hello, " + name_in.value : "no input");
+  }
+  try {
+    const url='https://jsonplaceholder.typicode.com/posts';
+      success: fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(jsonResponse) {
+        var posts = "";
+        jsonResponse.forEach((item) => {
+          posts += "<li>" + item["title"] + "</li>";
+        })
+        post_list.innerHTML = posts;
+      });
+  } catch(e) {
+    post_list.innerHTML = "<p>Unable to load posts</p>";
   }
 }
